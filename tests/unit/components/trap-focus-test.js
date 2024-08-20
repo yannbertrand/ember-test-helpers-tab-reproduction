47b272031b32cdf1d789482330a7d62e151c3efa
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-08-20 20:14:18 +0200

    remove module.only

--- tests/unit/components/trap-focus-test.js
+++ tests/unit/components/trap-focus-test.js
@@ -5,7 +5,7 @@ import { tap, tab } from '@ember/test-helpers';
 module('Unit | Component | pix-select', function (hooks) {
   setupTest(hooks);
 
-  module.only('#label', function () {
+  module('#label', function () {
     test('it should return the label if id is defined', function (assert) {
       assert.strictEqual(typeof tap, 'function', 'tap should be a function');
       assert.strictEqual(typeof tab, 'function', 'tab should be a function');
